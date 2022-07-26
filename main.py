5f05b67b7a42ae0aa981b0220fecb73226691503
Robert Norwood <you@example.com>
2022-07-26 13:35:04 -0700

    Added color to the buttons. Created the convert and clear_input functions. Program is working how it's supposed to.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import tkinter
 
+def convert():
+    converted_num = round(float(input.get()) * 1.6)
+    number_label['text'] = converted_num
+    return converted_num
+
+def clear_input():
+    input.delete(0, tkinter.END)
+
+
 window = tkinter.Tk()
 window.title('Mile to Km Converter')
 window.minsize(width=500, height=300)
@@ -8,7 +17,6 @@ window.config(padx=125, pady=100)
 
 input = tkinter.Entry()
 input.grid(column=1, row=0)
-
 
 mile_label = tkinter.Label(text='Miles')
 mile_label.grid(column=2, row=0)
@@ -27,9 +35,9 @@ km_label = tkinter.Label(text='Km')
 km_label.grid(column=2, row=1)
 km_label.config(pady=10)
 
-button = tkinter.Button(text='Calculate')
-button.grid(column=1, row=2)
-button.config(pady=10)
+convert_button = tkinter.Button(text='Convert', command=convert, bg='light blue')
+convert_button.grid(column=1, row=2)
 
-
+clear_button = tkinter.Button(text='Clear', command=clear_input, bg='light pink')
+clear_button.grid(column=2, row=2)
 window.mainloop()
